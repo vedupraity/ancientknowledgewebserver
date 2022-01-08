812c2b92082eeda2466657673b98e8eb9b80bd98
Vedprakash Upraity <vedprakashupraity@gmail.com>
2022-01-08 09:51:43 +0530

    build optimisation

--- freeze.py
+++ freeze.py
@@ -19,12 +19,17 @@ app.config['FREEZER_DESTINATION_IGNORE'] = ['*']
 app.config['FREEZER_DESTINATION'] = build_destionation
 app.config['FREEZER_DEFAULT_MIMETYPE'] = 'text/html; charset=utf-8'
 app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
-max_threads = 2
+app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
+app.config['FREEZER_SKIP_EXISTING'] = True
+max_threads = 3
+
+urls_frozen = 0
+
 
 class freezerClass():
-    def __init__(self, url):
+    def __init__(self, url, with_static_files):
         self.url = url
-        self.freezer = Freezer(app)
+        self.freezer = Freezer(app, with_static_files)
 
     def _yield_urls(self):
         print(f'generating {self.url}')
@@ -35,8 +40,8 @@ class freezerClass():
         self.freezer.freeze()
 
 
-def freeze_single_url(url):
-    freezer = freezerClass(url)
+def freeze_single_url(url, with_static_files):
+    freezer = freezerClass(url, with_static_files)
     freezer.freeze()
 
 
@@ -44,8 +49,10 @@ def freeze_urls_asynchronous(urls):
     threads = []
 
     for url in urls:
+        global urls_frozen
+        urls_frozen += 1
         threads.append(threading.Thread(
-            target=freeze_single_url, args=(url,)))
+            target=freeze_single_url, args=(url, False,)))
 
     for thread in threads:
         thread.start()
@@ -56,7 +63,9 @@ def freeze_urls_asynchronous(urls):
 
 def freeze_urls_synchronous(urls):
     for url in urls:
-        freeze_single_url(url)
+        global urls_frozen
+        urls_frozen += 1
+        freeze_single_url(url, True)
 
 
 def clean_build_directory(directory):
@@ -67,10 +76,10 @@ def clean_build_directory(directory):
 
 def get_content_urls():
     urls = []
-    
+
     github_database_tree = fetch_gitub_database_tree()
     languages = SITE_CONFIG['content_languages_codes']
-    
+
     for language in languages:
         language_prefix = f'/{language}/'
         for node in github_database_tree['tree']:  # todo
@@ -79,22 +88,36 @@ def get_content_urls():
     return urls
 
 
-def get_urls_to_freeze():
-    urls = ['/', '/about/', '/404/']
+def get_site_urls():
+    urls = ['/']
     urls += [f'/{language}/' for language in SITE_CONFIG['content_languages_codes']]
-    urls += get_content_urls()
     return urls
 
 
 if __name__ == '__main__':
     clean_build_directory(build_destionation)
 
-    urls = list(set(get_urls_to_freeze()))
-    print(f'freezing {len(urls)} urls')
-    
-    for i in range(0, len(urls), max_threads):
-        _urls = urls[i:i+max_threads]
+    site_urls = get_site_urls()
+    content_urls = get_content_urls()
+
+    total_url_count = len(site_urls) + len(content_urls)
+
+    print(f'freezing {total_url_count} urls')
+
+    # freeze website urls with static files
+    freeze_urls_synchronous(site_urls)
+
+    # freeze content urls without static files
+    for i in range(0, len(content_urls), max_threads):
+        _urls = content_urls[i:i+max_threads]
         freeze_urls_asynchronous(_urls)
 
     end_time = datetime.datetime.now()
-    print(f'took {(end_time - start_time).seconds} seconds to freeze {len(urls)} urls')
+    duration = end_time - start_time
+    days, seconds = duration.days, duration.seconds
+    
+    print(
+        f'Freeze ran for ' + \
+        f'{days * 24 + seconds // 3600}h:{(seconds % 3600) // 60}m:{seconds % 60}s ' + \
+        f'to freeze {urls_frozen} urls'
+    )
