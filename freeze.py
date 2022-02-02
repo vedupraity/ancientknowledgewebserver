ee81ad701de07492442b64da2296cfb34f5e0e95
Vedprakash Upraity <vedprakashupraity@gmail.com>
2022-02-02 09:45:06 +0530

    Update freeze.py

--- freeze.py
+++ freeze.py
@@ -1,4 +1,5 @@
 import datetime
+import math
 import os
 import shutil
 
@@ -9,63 +10,59 @@ from app import app
 from config import SITE_CONFIG
 from helpers.api import fetch_gitub_database_tree
 
-
 start_time = datetime.datetime.now()
 
 app_root_path = app.root_path
 build_destionation = 'build'
-app.config['FREEZER_REMOVE_EXTRA_FILES'] = False
-app.config['FREEZER_DESTINATION_IGNORE'] = ['*']
-app.config['FREEZER_DESTINATION'] = build_destionation
-app.config['FREEZER_DEFAULT_MIMETYPE'] = 'text/html; charset=utf-8'
-app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
-app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
-app.config['FREEZER_SKIP_EXISTING'] = True
 max_threads = 4
-
 urls_frozen = 0
 
 
-class freezerClass():
-    def __init__(self, url, with_static_files):
-        self.url = url
-        self.freezer = Freezer(app, with_static_files)
+class URLFreezer():
+    def __init__(self, urls, with_static_files=False):
+        flask_app = app
+        
+        app.config['FREEZER_REMOVE_EXTRA_FILES'] = False
+        app.config['FREEZER_DESTINATION_IGNORE'] = ['*']
+        app.config['FREEZER_DESTINATION'] = build_destionation
+        app.config['FREEZER_DEFAULT_MIMETYPE'] = 'text/html; charset=utf-8'
+        app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
+        app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
+        app.config['FREEZER_SKIP_EXISTING'] = True
+        
+        self.urls = urls
+        self.freezer = Freezer(flask_app, with_static_files)
 
-    def _yield_urls(self):
-        print(f'generating {self.url}')
-        yield self.url
+    def yield_urls(self):
+        for url in self.urls:
+            print(f'freezing url {url}')
+            global urls_frozen
+            urls_frozen += 1
+            yield url
 
     def freeze(self):
-        self.freezer.register_generator(self._yield_urls)
+        self.freezer.register_generator(self.yield_urls)
         self.freezer.freeze()
 
 
-def freeze_single_url(url, with_static_files):
-    freezer = freezerClass(url, with_static_files)
-    freezer.freeze()
-
-
-def freeze_urls_asynchronous(urls):
+def freeze_urls_asynchronous(urls, with_static_files, max_threads):
+    urls_per_thread = math.ceil(len(urls) / max_threads)
     threads = []
 
-    for url in urls:
-        global urls_frozen
-        urls_frozen += 1
-        threads.append(threading.Thread(
-            target=freeze_single_url, args=(url, False,)))
+    for i in range(0, len(urls), urls_per_thread):
+        urls_chunk = urls[i:i+urls_per_thread]
+        freezer = URLFreezer(urls_chunk, with_static_files)
+        threads.append(
+            threading.Thread(target=freezer.freeze)
+        )
+
+    print(f'Running {len(threads)} threads for {len(urls)}')
 
     for thread in threads:
         thread.start()
 
     for thread in threads:
         thread.join()
-
-
-def freeze_urls_synchronous(urls):
-    for url in urls:
-        global urls_frozen
-        urls_frozen += 1
-        freeze_single_url(url, True)
 
 
 def clean_build_directory(directory):
@@ -107,26 +104,24 @@ if __name__ == '__main__':
     clean_build_directory(build_destionation)
 
     site_urls = get_site_urls()
-    content_urls = get_content_urls()
+    content_urls = get_content_urls()[:20]
 
     total_url_count = len(site_urls) + len(content_urls)
 
     print(f'freezing {total_url_count} urls')
 
     # freeze website urls with static files
-    freeze_urls_synchronous(site_urls)
+    freeze_urls_asynchronous(site_urls, with_static_files=True, max_threads=1)
 
     # freeze content urls without static files
-    for i in range(0, len(content_urls), max_threads):
-        _urls = content_urls[i:i+max_threads]
-        freeze_urls_asynchronous(_urls)
+    freeze_urls_asynchronous(
+        content_urls, with_static_files=False, max_threads=max_threads)
 
     end_time = datetime.datetime.now()
     duration = end_time - start_time
     days, seconds = duration.days, duration.seconds
 
     print(
-        f'Freeze ran for ' +
-        f'{days * 24 + seconds // 3600}h:{(seconds % 3600) // 60}m:{seconds % 60}s ' +
-        f'to freeze {urls_frozen} urls'
+        f'Generated static pages for {urls_frozen}/{total_url_count} urls in ' +
+        f'{days * 24 + seconds // 3600}h:{(seconds % 3600) // 60}m:{seconds % 60}s '
     )
